d9069dbe4535092fb996a75bdb8776458b7fd146
TixAinan <tix.ainan@gmail.com>
2024-11-03 13:36:24 +0100

    csv of municipaly number, continued work on loading data and connecting to mysql server

--- src/energydata_loading.py
+++ src/energydata_loading.py
@@ -1,17 +1,52 @@
-import requests
+from dotenv import load_dotenv
+import sys
+import os
+import mysql.connector
 import pandas as pd
 
-response = requests.get(
-    url='https://api.energidataservice.dk/dataset/ProductionMunicipalityHour?offset=0&start=2024-10-01T00:00&end=2024-11-01T00:00&sort=HourUTC%20DESC&limit=500000')
+sys.path.append("src") 
+import dataloading_functions as dl
 
-result = response.json()
+#Loading .env
+load_dotenv()
 
-records = result.get('records', [])
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+db_port = os.getenv("DB_PORT")
 
-df = pd.DataFrame(records)
+try:
+    connection = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_name,
+        port = db_port
+    )
+
+    if connection.is_connected():
+        print("Connection to MySQL was successful!")
+    connection.close()
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+
+## Define the data we want from the api and send a get request
+
+api_url = 'https://api.energidataservice.dk/dataset/ProductionMunicipalityHour?offset=0&start=2024-10-01T00:00&end=2024-11-01T00:00&sort=HourUTC%20DESC&limit=50'  #energidata url
+
+df = dl.get_data_from_api(api_url)
 
 df.describe()
 
 df = df.sort_values(by=['MunicipalityNo','HourUTC'])
 
+df.info()
 df.head(50)
+
+df.MunicipalityNo.unique()
+
+mun_no = pd.read_csv(r"MunicipalityNo.csv")
+
+mun_no.info()
